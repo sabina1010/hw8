de60b7355b70c2cccb667adc75620fa1df8c27d5
sabina1010 <109016620+sabina1010@users.noreply.github.com>
2022-08-30 23:41:44 +0400

    Update PetTest.java

--- hw8/src/test/java/company/PetTest.java
+++ hw8/src/test/java/company/PetTest.java
@@ -5,6 +5,9 @@ import company.pets.DomesticCat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PetTest {
@@ -12,19 +15,21 @@ class PetTest {
 
     @BeforeEach
     void init() {
-        cat = new DomesticCat("Charlie", 2, 1, new String[]{"eating", "sleeping"});
+        cat = new DomesticCat("Charlie", 2, 1, Set.of("eating", "sleeping"));
 
     }
 
     @Test
     void testPetEquality() {
-        Pet cat2 = new DomesticCat( "Charlie", 2, 1, new String[]{"eating", "sleeping"});
+        Set<String> habits = new HashSet<>(Set.of("eating", "sleeping"));
+        Pet cat2 = new DomesticCat( "Charlie", 2, 1, habits);
         assertEquals(cat, cat2);
     }
 
     @Test
     void testPetNonEquality(){
-        Pet cat2 = new DomesticCat ("Charlie", 1, 1, new String[]{"eating", "sleeping"});
+        Set<String> habits = new HashSet<>(Set.of("sleeping", "eating"));
+        Pet cat2 = new DomesticCat ("Charlie", 1, 1,habits);
         assertNotEquals(cat, cat2);
     }
 
